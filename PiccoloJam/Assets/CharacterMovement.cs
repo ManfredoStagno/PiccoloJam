855b4f1d1a15abe88f75f61528fe9edd9d109e94
Egon <egon.cavalli@gmail.com>
2018-01-27 13:01:23 +0100

    fabio brigidi scansati che so arrivato c'è un nuovo programmatore in città

--- PiccoloJam/Assets/CharacterMovement.cs
+++ PiccoloJam/Assets/CharacterMovement.cs
@@ -6,18 +6,17 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-
 	//time and input delay Variables
 	public float delay = 5f;
-	List <float> axis = new List<float>();
+	List <float> haxis = new List<float>();
+	List <float> vaxis = new List<float>();
 
 	//movement and controller variables
-
-	public float currentAxis;
+	public float currentHAxis;
+	public float currentVAxis;
 	public float moveForce = 365f;
 	public float maxSpeed = 5f;
 	public Rigidbody2D rb2d;
-
 
 	void Start()
 	{
@@ -25,33 +24,45 @@ public class CharacterMovement : MonoBehaviour {
 
 		for (int i = 0; i < delay*50; i++) 
 		{
-			axis.Add (0);
+			haxis.Add (0);
+			vaxis.Add (0);
 		}
-
 	}
 
 
 	void Update () 
 	{
-		axis.Add(Input.GetAxisRaw("Horizontal"));
+		haxis.Add(Input.GetAxisRaw("Horizontal"));
+		vaxis.Add(Input.GetAxisRaw("Vertical"));
 	}
 
 	void FixedUpdate()
 	{
 		{
-			currentAxis = axis [1];
+			currentHAxis = haxis [1];
 
 				//movimento
-				if (rb2d.velocity.x * currentAxis < maxSpeed) 
-					rb2d.AddForce (Vector2.right * currentAxis * moveForce);
+				if (rb2d.velocity.x * currentHAxis < maxSpeed) 
+					rb2d.AddForce (Vector2.right * currentHAxis * moveForce);
 
 				if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
 							rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 				//movimento
 
-			axis.RemoveAt(1);
+			haxis.RemoveAt(1);
+		}
+		{
+			currentVAxis = vaxis [1];
 
+			//movimento
+			if (rb2d.velocity.y * currentVAxis < maxSpeed) 
+				rb2d.AddForce (Vector2.up * currentVAxis * moveForce);
 
+			if (Mathf.Abs (rb2d.velocity.y) > maxSpeed)
+				rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign (rb2d.velocity.y) * maxSpeed);
+			//movimento
+
+			vaxis.RemoveAt(1);
 		}
 	}
 }
