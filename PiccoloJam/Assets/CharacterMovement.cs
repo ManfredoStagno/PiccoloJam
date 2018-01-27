66c38cc6b5f9a51ed1f7f9a36d5b961d0c131ca4
Egon <egon.cavalli@gmail.com>
2018-01-27 12:27:03 +0100

    inglese sta cippa e robb me fa na pippa

--- PiccoloJam/Assets/CharacterMovement.cs
+++ PiccoloJam/Assets/CharacterMovement.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +8,14 @@ public class CharacterMovement : MonoBehaviour {
 
 
 	//time and input delay Variables
-	public float timer = 0;
-	public float delay = 2f;
-	public bool isPressed = false;
-	public int timePressed;
+	public float delay = 5f;
+	List <float> axis = new List<float>();
 
 	//movement and controller variables
-	public float axis;
-	public float moveForce = 250f;
-	public float maxSpeed = 500f;
+
+	public float currentAxis;
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
 	public Rigidbody2D rb2d;
 
 
@@ -22,49 +23,35 @@ public class CharacterMovement : MonoBehaviour {
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 
+		for (int i = 0; i < delay*50; i++) 
+		{
+			axis.Add (0);
+		}
+
 	}
 
 
 	void Update () 
 	{
-		//check if any button is pressed
-		if (Input.GetAxis ("Horizontal") !=0)
-		{
-			axis = Input.GetAxis ("Horizontal");
-			Debug.Log ("press");
-			isPressed = true;
-			timePressed++;
-
-		}
+		axis.Add(Input.GetAxisRaw("Horizontal"));
 	}
 
 	void FixedUpdate()
 	{
-		//if something was rpessed
-		if (isPressed == true)
 		{
-			timer++;
+			currentAxis = axis [1];
 
-			//if the time has passed
-			if (timer >= 50*delay)
-			{
-				if (rb2d.velocity.x * axis < maxSpeed) 
-				{
-					rb2d.AddForce (Vector2.right * axis * moveForce);
-				}
+				//movimento
+				if (rb2d.velocity.x * currentAxis < maxSpeed) 
+					rb2d.AddForce (Vector2.right * currentAxis * moveForce);
 
-				Debug.Log (rb2d.velocity.x);
-				Debug.Log("movement");
-				timePressed--;
+				if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+							rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+				//movimento
 
-				//if the input has been processed
-				if(timePressed <= 0)
-					{
-					isPressed = false;
-					timePressed = 0;
-					timer = 0;	
-					}
-			}
+			axis.RemoveAt(1);
+
+
 		}
 	}
 }
