1536468f131ccd6d34315b9bea56cf4e21fc3dca
Egon <egon.cavalli@gmail.com>
2018-01-27 21:44:40 +0100

    ti pusho tutto <3

--- PiccoloJam/Assets/CharacterMovement.cs
+++ PiccoloJam/Assets/CharacterMovement.cs
@@ -6,25 +6,19 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-
 	//time and input delay Variables
-
 	public float delay = 5f;
 	List <float> haxis = new List<float>();
 	List <float> vaxis = new List<float>();
-
-	//movement and controller variables
 	public float currentHAxis;
 	public float currentVAxis;
-
+	//movement and controller variables
 	public float moveForce = 365f;
-
 	public float maxSpeed = 5f;
 	private Rigidbody2D rb2d;
-
 	//jump
-//	public Transform groundCheck;
 	public bool grounded = false;
+	public float jumpForce = 365f;
 
 	void Start()
 	{
@@ -37,27 +31,12 @@ public class CharacterMovement : MonoBehaviour {
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D coll)
-	{
-		if (coll.gameObject.CompareTag ("Ground")) 
-		{
-			grounded = true;
-		}
-	}
-
-	void OnCollisionExit2D(Collision2D coll)
-	{
-		if (coll.gameObject.CompareTag ("Ground")) 
-		{
-			grounded = false;
-		}
-	}
 
 	void Update () 
 	{
 		haxis.Add(Input.GetAxisRaw("Horizontal"));
-		vaxis.Add(Input.GetAxisRaw("Vertical"));
-		//grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("ground"));
+
+		vaxis.Add(Input.GetAxis("Vertical"));
 	}
 
 	void FixedUpdate()
@@ -75,19 +54,52 @@ public class CharacterMovement : MonoBehaviour {
 
 			haxis.RemoveAt(1);
 		}
+		if(grounded == true)
 		{
 			currentVAxis = vaxis [1];
 
 			//movimento
 			if (rb2d.velocity.y * currentVAxis < maxSpeed) 
-				rb2d.AddForce (Vector2.up * currentVAxis * moveForce);
-			Debug.Log ("Jumping");
+				rb2d.AddForce (Vector2.up * currentVAxis * jumpForce);
+		
 
 			if (rb2d.velocity.y > maxSpeed)
 				rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign (rb2d.velocity.y) * maxSpeed);
 			//movimento
-			vaxis.RemoveAt(1);
-			Debug.Log ("Bouncing");
+		}
+		vaxis.RemoveAt(1);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	//TODO: death function
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.CompareTag ("Ground")) 
+		{
+			grounded = true;
+		}
+		if (coll.gameObject.CompareTag ("Ground")) 
+		{
+			grounded = true;
 		}
 	}
+	
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.CompareTag ("Ground")) 
+		{
+			grounded = false;
+		}
+	}
+
 }
