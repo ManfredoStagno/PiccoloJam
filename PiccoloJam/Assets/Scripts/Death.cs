77ed5b5af20012bc2bc6f68a7c8a1378a5287e7f
saem <chionettisamuele@gmail.com>
2018-01-28 11:05:19 +0100

    death

--- PiccoloJam/Assets/Scripts/Death.cs
+++ PiccoloJam/Assets/Scripts/Death.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
+using UnityEditor;
+using UnityEngine.SceneManagement;
 
-public class Death : MonoBehaviour {
+public class Death : MonoBehaviour{
+
+	private Animator anim;
+	private float DeathTime;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		anim = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Time.time >= DeathTime + 3) 
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+				}
 	}
 
 	public void InstaDeath ()
 
 	{
-		
+		anim.Play("Death"); 
+		DeathTime = Time.time;
 	}
 }
