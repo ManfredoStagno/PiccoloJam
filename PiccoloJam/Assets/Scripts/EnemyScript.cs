76db077404d7796703d0dfd428aedd7d776e1881
Egon <egon@MacBook-Pro-di-Egon.local>
2018-01-28 11:25:12 +0100

    Cazzo numeri da circo Orfei con Morfeo che chiama

---
+++ PiccoloJam/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public Sprite laser;
+	Animator anim;
+	private int t1 = 0;
+	public int t2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		//anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		t1++;
+		if (t1 >= t2) 
+		{
+			Debug.Log ("yes!wecand");
+			anim = GetComponent<Animator> ();
+			Animator.StringToHash ("lasergo");
+			t1 = 0;
+		} 
+
+	}
+		
+}
