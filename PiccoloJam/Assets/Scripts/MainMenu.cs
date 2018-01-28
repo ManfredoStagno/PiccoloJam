980c664731f7b740bdecbb514640a0a4e7f5abb0
Egon <egon@MacBook-Pro-di-Egon.local>
2018-01-28 03:03:50 +0100

    Maneggio contrario di Pedaggio?

---
+++ PiccoloJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string startLevel;
+
+
+	public void NewGame()
+	{
+		SceneManager.LoadScene ("Scena01");
+	}
+		
+}
